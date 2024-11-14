1b501910b73ea793e1eb27919e7ea85a3a13c123
Davi <davimartinsguedes@hotmail.com>
2024-11-14 20:55:43 -0300

    implementado: login
    implementado o login

--- login_cliente_function.js
+++ login_cliente_function.js
@@ -1,30 +1,47 @@
+import { Database } from "./services";
+
 // Função para abrir o formulário de login
 function openLoginForm() {
-    document.getElementById("loginForm").style.display = "block";
-  }
+  document.getElementById("loginForm").style.display = "block";
+}
   
   // Função para fechar o formulário de login
-  function closeLoginForm() {
-    document.getElementById("loginForm").style.display = "none";
-  }
+function closeLoginForm() {
+  document.getElementById("loginForm").style.display = "none";
+}
   
-  function login() {
-    const cpf = document.getElementById("loginCPF").value;
-    const password = document.getElementById("loginPassword").value;
-  
-    const clientData = localStorage.getItem(`client_${cpf}`);
-  
-    if (clientData) {
-      const client = JSON.parse(clientData);
-  
-      if (client.password === password) {
-        alert("Login realizado com sucesso!");
-        localStorage.setItem("loggedInClient", cpf); // Define o usuário como logado
-        window.location.href = "index.html"; // Redireciona para a página inicial
-      } else {
-        alert("Senha incorreta. Tente novamente.");
-      }
+function login() {
+  const cpf = document.getElementById("loginCPF").value;
+  const password = document.getElementById("loginPassword").value;
+
+  const client = Database.getPersonByTag("CPF",cpf);
+
+  if (client) {
+    if (client.senha === password) {
+      alert("Login realizado com sucesso!");
+      window.location.href = "index.html"; // Redireciona para a página inicial
     } else {
-      alert("CPF não encontrado. Verifique seus dados ou cadastre-se.");
+      alert("Senha incorreta. Tente novamente.");
     }
+  } else {
+    alert("CPF não encontrado. Verifique seus dados ou cadastre-se.");
+  };
+
+  /*
+  const clientData = localStorage.getItem(`client_${cpf}`);
+  
+  if (clientData) {
+    const client = JSON.parse(clientData);
+
+    if (client.password === password) {
+      alert("Login realizado com sucesso!");
+      localStorage.setItem("loggedInClient", cpf); // Define o usuário como logado
+      window.location.href = "index.html"; // Redireciona para a página inicial
+    } else {
+      alert("Senha incorreta. Tente novamente.");
+    }
+  } else {
+    alert("CPF não encontrado. Verifique seus dados ou cadastre-se.");
   }
+  */
+}
--- services.js
+++ services.js
@@ -3,9 +3,20 @@ import { createClient } from '@supabase/supabase-js'
 const key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1eGRhY2dvbXVieHZyeGpqdWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA3Mjg2NzMsImV4cCI6MjA0NjMwNDY3M30.RD-izZhI0GYmUNrOHtRrFYjh7lRBuabjDoQGgJZxPuk;
 const url='https://juxdacgomubxvrxjjuhg.supabase.co';
 const supabaseKey = process.env.SUPABASE_KEY;
-const database = createClient(url,key);
+const supabase = createClient(url,key);
 
-export const database_services = {
+export const utils = {
+    Input: {
+        constructor(name) {
+            this.name = name;
+        },
+        get query() {
+            return document.querySelector(this.name)
+        }
+    }
+}
+
+export const Database = {
     Person: {
         constructor(nome,cpf,email,telefone,senha) {
             this.nome = nome;
@@ -25,7 +36,7 @@ export const database_services = {
         }
     },
     add_to_Pessoa: async (person) => {
-        let res = await database.from("TB_PESSOA").insert({
+        let res = await supabase.from("TB_PESSOA").insert({
             NOME: person.getData().nome,
             CPF: person.getData().cpf,
             EMAIL: person.getData().email,
@@ -38,35 +49,72 @@ export const database_services = {
             alert("Pessoa não pode ser adicionada!")
         };
     },
+    getPersonByTag: async (tag,value) => {
+        let res = await supabase.from("TB_PESSOA").select("*").eq(tag,value);
+        if (res) {
+            return {
+                nome: res.data[0].NOME,
+                cpf: res.data[0].CPF,
+                email: res.data[0].EMAIL,
+                telefone: res.data[0].TELEFONE,
+                senha: res.data[0].SENHA
+            }
+        } else {
+            return false;
+        }
+    },
     Profissional: {
-        constructor(carteira,especialidade,email,senha,region,links) {
-            this.carteira = carteira;
+        constructor(nome,crm,especialidade,telefone,email,senha,instagram,facebook) {
+            this.nome = nome;
+            this.crm = crm;
             this.especialidade = especialidade;
+            this.telefone = telefone;
             this.email = email;
             this.senha = senha;
-            this.region = region;
-            this.links = links;
+            this.instagram = instagram;
+            this.facebook = facebook;
         },
         get getData() {
             return {
+                nome: this.nome,
                 carteira: this.carteira,
                 especialidade: this.especialidade,
+                telefone: this.telefone,
                 email: this.email,
                 senha: this.senha,
-                region: this.region,
-                links: this.links
+                instagram: this.instagram,
+                facebook: this.facebook
             }
         }
     },
     add_to_Profissional: async (person) => {
-        let res = await database.from("TB_PROFISSIONAL").insert({
-            N_CARTEIRA_CLINICA: person.getData().carteira,
+        let res = await supabase.from("TB_PROFISSIONAL").insert({
+            NOME: person.getData().nome,
+            CRM: person.getData().crm,
             ESPECIALIDADE: person.getData().especialidade,
-            EMAIL: person.getData().email,
             TELEFONE: person.getData().telefone,
+            EMAIL: person.getData().email,
             SENHA: person.getData().senha,
-            TB_REGIAO: person.getData().region,
-            TX_LINKS: person.getData().links
+            FACEBOOK: person.getData().facebook,
+            INSTAGRAM: person.getData().instagram
         });
+    },
+    getProfByTag: async (tag,value) => {
+        const res = await supabase.from("TB_PROFISSIONAL").select("*").eq(tag,value);
+        if (res) {
+            return {
+                nome: res.data[0].NOME,
+                cpf: res.data[0].CPF,
+                carteira: res.data[0].N_CARTEIRA_CLINICA,
+                especialidade: res.data[0].ESPECIALIDADE,
+                email: res.data[0].EMAIL,
+                telefone: res.data[0].TELEFONE,
+                senha: res.data[0].SENHA,
+                regiao: res.data[0].TB_REGIAO,
+                links: res.data[0].TX_LINKS
+            }
+        } else {
+            return false;
+        }
     }
 }
